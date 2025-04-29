4942f24743a9f9b444bad709806142f160fff086
vitali.rulinski <vitalrulinski@gmail.com>
2025-04-29 08:31:30 +0200

    refactor: Remove HelloWorld event implementation.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Events\HelloWorld;
 use App\Events\MessageReceived;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
@@ -8,7 +7,6 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    HelloWorld::dispatch();
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
